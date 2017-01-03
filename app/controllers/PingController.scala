bd8556983b2d1eac726f308c3d07c4db69bfc4d7
Dan Fithian <dan.fithian@tvisioninsights.com>
2017-01-03 11:46:51 -0800

    Unit -> empty string

--- app/controllers/PingController.scala
+++ app/controllers/PingController.scala
@@ -1,14 +1,11 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import akka.util.ByteString
-import play.api.http.Writeable
 import play.api.mvc.{Action, Controller}
 
 @Singleton
 class PingController @Inject() extends Controller {
-  implicit def unitWriteable: Writeable[Unit] = new Writeable(_ => ByteString("{}"), None)
   def ping = Action { implicit request =>
-    Ok(())
+    Ok("")
   }
 }
